ccaa18ac55aee0bfab6e72d9d11ecb3cbe55d664
Sámuel Fekete <feketesamu@gmail.com>
2022-10-19 21:06:01 +0200

    mongoose init from out of nowhere

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -20,6 +20,7 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/strict-boolean-expressions': 'off',
     '@typescript-eslint/prefer-nullish-coalescing': 'off',
-    '@typescript-eslint/no-misused-promises': 'off'
+    '@typescript-eslint/no-misused-promises': 'off',
+    '@typescript-eslint/no-floating-promises': 'off'
   }
 }
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,7 @@ import express, { Express } from 'express'
 import dotenv from 'dotenv'
 import routing from './routing'
 import cors from 'cors'
+import mongoose from 'mongoose'
 
 dotenv.config()
 
@@ -11,6 +12,9 @@ const port = process.env.PORT
 app.use(cors())
 
 routing(app)
+
+mongoose.connect(`mongodb+srv://${process.env.DB_USER || ''}:${process.env.DB_PWD || ''}
+    @cluster0.4evepic.mongodb.net/?retryWrites=true&w=majority`, () => console.log('Connected to db!'))
 
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port || 8000}`)
