1694cd865910965ce5180fbd4f2ff8d06777ed38
Sámuel Fekete <feketesamu@gmail.com>
2022-10-21 13:04:59 +0200

    auth middleware finalized

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -14,7 +14,7 @@ app.use(cors())
 routing(app)
 // eslint-disable-next-line max-len
 const connStr = `mongodb+srv://${process.env.DB_USER || ''}:${process.env.DB_PWD || ''}@cluster0.4evepic.mongodb.net/?retryWrites=true&w=majority`
-mongoose.connect(connStr).catch((e: Error) => console.error(e))
+mongoose.connect(connStr).then(() => console.log('Connected to db!')).catch((e: Error) => console.error(e))
 
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port || 8000}`)
--- backend/src/routing/index.ts
+++ backend/src/routing/index.ts
@@ -1,5 +1,5 @@
 import { Express, Request, Response } from 'express'
-import { decodeToken, registerUser } from '../middleware'
+import { decodeToken, getUserByUId, registerUser } from '../middleware/auth'
 import delivery from './delivery'
 import user from './user'
 import vehicle from './vehicle'
@@ -8,7 +8,7 @@ export default (app: Express) => {
   app.get('/', (req: Request, res: Response) => {
     res.send('A szállítók')
   })
-  app.get('/guarded', decodeToken, registerUser)
+  app.get('/login', decodeToken, getUserByUId, registerUser)
   delivery(app)
   user(app)
   vehicle(app)
