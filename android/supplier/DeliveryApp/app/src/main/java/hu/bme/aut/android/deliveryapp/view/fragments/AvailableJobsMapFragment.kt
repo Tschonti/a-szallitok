6d709deaea27cded513f9ef09a4424249f537d5c
domonkosadam <domonkosadam01@gmail.com>
2022-10-31 22:36:30 +0100

    Supplier map integrated

--- android/supplier/DeliveryApp/app/src/main/java/hu/bme/aut/android/deliveryapp/view/fragments/AvailableJobsMapFragment.kt
+++ android/supplier/DeliveryApp/app/src/main/java/hu/bme/aut/android/deliveryapp/view/fragments/AvailableJobsMapFragment.kt
@@ -1,51 +1,84 @@
 package hu.bme.aut.android.deliveryapp.view.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import com.devhoony.lottieproegressdialog.LottieProgressDialog
 import com.example.awesomedialog.*
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import hu.bme.aut.android.deliveryapp.R
-import hu.bme.aut.android.deliveryapp.databinding.FragmentAvailableJobsMapBinding
+import hu.bme.aut.android.deliveryapp.model.JobDetails
 import hu.bme.aut.android.deliveryapp.view.states.JobDetailState
 import hu.bme.aut.android.deliveryapp.viewmodel.AvailableJobsMapFragmentViewModel
 
 class AvailableJobsMapFragment : Fragment() {
 
-    private lateinit var binding: FragmentAvailableJobsMapBinding
-
     private val viewModel: AvailableJobsMapFragmentViewModel by viewModels()
 
+    private lateinit var loadingDialog: LottieProgressDialog
+
+    private lateinit var googleMap: GoogleMap
+
+    private val jobList: MutableList<JobDetails> = mutableListOf()
+
+    private val callback = OnMapReadyCallback { googleMap ->
+        this.googleMap = googleMap
+    }
+
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        binding = FragmentAvailableJobsMapBinding.inflate(layoutInflater, container, false)
-        return binding.root
+        return inflater.inflate(R.layout.fragment_available_jobs_map, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
+        mapFragment?.getMapAsync(callback)
 
         viewModel.getAvailableJobs(null, null,null, null).observe(viewLifecycleOwner
         ) { jobDetailState ->
             render(jobDetailState)
         }
+
+        loadingDialog = LottieProgressDialog(
+            context = requireContext(),
+            isCancel = true,
+            dialogWidth = null,
+            dialogHeight = null,
+            animationViewWidth = null,
+            animationViewHeight = null,
+            fileName = LottieProgressDialog.SAMPLE_8,
+            title = null,
+            titleVisible = null
+        )
     }
 
     private fun render(state: JobDetailState) {
         when (state) {
             is JobDetailState.inProgress -> {
-                Toast.makeText(context, "Loading Data", Toast.LENGTH_SHORT).show()
+                loadingDialog.show()
             }
             is JobDetailState.jobDetailsResponseSuccess -> {
-                binding.tvText.text = state.data.toString()
+                loadingDialog.dismiss()
+                state.data.forEach {
+                    jobList.add(it)
+                    addMarker(it)
+                }
+                googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(LatLng(jobList[0].deliveryLocation!!.coordinate.latitude.toDouble(), jobList[0].deliveryLocation!!.coordinate.longitude.toDouble()), 10f))
             }
             is JobDetailState.jobDetailsResponseError -> {
+                loadingDialog.dismiss()
                 AwesomeDialog.build(requireActivity())
                     .title("Error")
                     .body(state.exceptionMsg)
@@ -55,4 +88,10 @@ class AvailableJobsMapFragment : Fragment() {
         }
     }
 
+    private fun addMarker(job: JobDetails) {
+        val markerOptions = MarkerOptions()
+        markerOptions.position(LatLng(job.deliveryLocation!!.coordinate.latitude.toDouble(), job.deliveryLocation.coordinate.longitude.toDouble()))
+        markerOptions.title(job.title)
+        googleMap.addMarker(markerOptions)
+    }
 }
