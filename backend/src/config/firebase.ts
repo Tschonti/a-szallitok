f1bdaada8b2f911a661add17cfe9464962d43f58
Sámuel Fekete <feketesamu@gmail.com>
2022-11-13 15:05:14 +0100

    na ha most se mukodsz...

--- backend/src/config/firebase.ts
+++ backend/src/config/firebase.ts
@@ -1,19 +1,22 @@
 import * as firebase from 'firebase-admin'
-import * as serviceAccount from './firebase-service-account.json'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 const params = {
-  type: serviceAccount.type,
-  projectId: serviceAccount.project_id,
-  privateKeyId: serviceAccount.private_key_id,
-  privateKey: serviceAccount.private_key,
-  clientEmail: serviceAccount.client_email,
-  clientId: serviceAccount.client_id,
-  authUri: serviceAccount.auth_uri,
-  tokenUri: serviceAccount.token_uri,
-  authProviderX509CertUrl: serviceAccount.auth_provider_x509_cert_url,
-  clientC509CertUrl: serviceAccount.client_x509_cert_url
+  type: 'service_account',
+  projectId: 'the-transporters-fb',
+  privateKeyId: process.env.PRIVATE_KEY_ID,
+  privateKey: process.env.PRIVATE_KEY,
+  clientEmail: 'firebase-adminsdk-y08ow@the-transporters-fb.iam.gserviceaccount.com',
+  clientId: process.env.CLIENT_ID,
+  authUri: 'https://accounts.google.com/o/oauth2/auth',
+  tokenUri: 'https://oauth2.googleapis.com/token',
+  authProviderX509CertUrl: 'https://www.googleapis.com/oauth2/v1/certs',
+  clientC509CertUrl:
+  // eslint-disable-next-line max-len
+  'https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-y08ow%40the-transporters-fb.iam.gserviceaccount.com'
 }
-
 export const app = firebase.initializeApp({
   credential: firebase.credential.cert(params)
 })
