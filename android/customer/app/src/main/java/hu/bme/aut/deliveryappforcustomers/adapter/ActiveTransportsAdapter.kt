320d357c5b14da7cdabef5ed11d0212ca1b3c6f4
Oliver Raisz <raiszoliver@gmail.com>
2022-11-14 13:03:30 +0100

    pending deliveries - WIP

---
+++ android/customer/app/src/main/java/hu/bme/aut/deliveryappforcustomers/adapter/ActiveTransportsAdapter.kt
@@ -0,0 +1,5 @@
+package hu.bme.aut.deliveryappforcustomers.adapter
+
+class ActiveTransportsAdapter {
+
+}
--- android/customer/app/src/main/java/hu/bme/aut/deliveryappforcustomers/view/fragments/ActiveTransportsFragment.kt
+++ android/customer/app/src/main/java/hu/bme/aut/deliveryappforcustomers/view/fragments/ActiveTransportsFragment.kt
@@ -1,23 +1,30 @@
 package hu.bme.aut.deliveryappforcustomers.view.fragments
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import hu.bme.aut.android.deliveryapp.model.Delivery
 import hu.bme.aut.deliveryappforcustomers.R
-import hu.bme.aut.deliveryappforcustomers.databinding.FragmentActiveTransportsBinding
-import hu.bme.aut.deliveryappforcustomers.view.JobDetailState
+import hu.bme.aut.deliveryappforcustomers.adapter.ActiveTransportsAdapter
+import hu.bme.aut.deliveryappforcustomers.repository.CurrentUser
+import hu.bme.aut.deliveryappforcustomers.repository.CurrentUser.user
+import hu.bme.aut.deliveryappforcustomers.viewmodel.ActiveTransportsViewModel
+import retrofit2.Response.error
 
 class ActiveTransportsFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = ActiveTransportsFragment()
-    }
+    private lateinit var binding: ActiveTransportsFragmentBinding
 
-    //private lateinit var viewModel: ActiveTransportsViewModel
-    private lateinit var binding: FragmentActiveTransportsBinding
+    private val viewModel: ActiveTransportsViewModel by viewModels()
+
+    private lateinit var adapter: ActiveTransportsAdapter
+
+    //private lateinit var loadingDialog: LottieProgressDialog
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -29,31 +36,44 @@ class ActiveTransportsFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        /*viewModel.getActiveTransports(10).observe(viewLifecycleOwner
+       adapter = JobDetailsAdapter(requireContext(), this)
+
+        //TODO change to valid id
+        viewModel.getActiveTransports(0/*CurrentUser.user._id*/).observe(viewLifecycleOwner
         ) { jobDetailState ->
             render(jobDetailState)
-        }*/
+        }
+
+        binding.availableJobsRecyclerView.adapter = adapter
     }
 
-    private fun render(state: JobDetailState) {
+   /* private fun render(state: TransportListState) {
         when (state) {
-            is JobDetailState.inProgress -> {
-                Toast.makeText(context, "Loading Data", Toast.LENGTH_SHORT).show()
+            is DeliveryListState.inProgress -> {
+                loadingDialog.show()
             }
-            is JobDetailState.jobDetailsResponseSuccess -> {
-                binding.tvText.text = state.data.toString()
+            is DeliveryListState.deliveriesResponseSuccess -> {
+                loadingDialog.dismiss()
+                Log.i("DATA ARRIVED", state.data.toString())
+                adapter.addJobs(state.data)
             }
-            is JobDetailState.jobDetailsResponseError -> {
-                Toast.makeText(context, "Error", Toast.LENGTH_SHORT).show()
+            is DeliveryListState.deliveriesResponseError -> {
+                loadingDialog.dismiss()
+                AwesomeDialog.build(requireActivity())
+                    .title("Error")
+                    .body(state.exceptionMsg)
+                    .icon(R.drawable.error)
+                    .onPositive("Close")
             }
         }
-    }
+    }*/
 
-
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        //viewModel = ViewModelProvider(this).get(ActiveTransportsViewModel::class.java)
-        // TODO: Use the ViewModel
+    override fun onTransportSelected(job: Delivery?) {
+     /*   Log.d("JOB", job.toString())
+        val b = Bundle()
+        b.putSerializable("JOB", job)
+        findNavController().navigate(R.id.action_availableJobsFragment_to_availableJobDetailsFragment, b)*/
+        // TODO: implement function
     }
 
 }
