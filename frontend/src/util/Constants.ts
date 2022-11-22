2d2e9d9a667716e5e0e4334ebd338425b75ff66c
Sámuel Fekete <feketesamu@gmail.com>
2022-11-22 22:50:19 +0100

    cascade delete user

--- frontend/src/util/Constants.ts
+++ frontend/src/util/Constants.ts
@@ -1,3 +1,4 @@
 export const COOKIE_KEY_JWT = 'JWT_TOKEN'
-export const BASE_URL = 'https://a-szallitok-api.azurewebsites.net/'
+//export const BASE_URL = 'https://a-szallitok-api.azurewebsites.net/'
+export const BASE_URL = 'http://localhost:8000'
 export const API_KEY = 'AIzaSyCwGG6uz2xFPevws6o_wRLed6Eiz-J71Xw'
